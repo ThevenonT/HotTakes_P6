fb02f959137cb00ace7581510ea02f05f2ceccfc
ThevenonT <thevenon.thomas@hotmail.com>
2022-08-07 11:24:17 +0200

    added comments and added the possibility to delete an element

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 // afficher tous les objet 
 exports.getAllSauce = (req, res, next) => {
+    //récupère toute les sauces présente 
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
@@ -11,15 +12,17 @@ exports.getAllSauce = (req, res, next) => {
 // enregistrer un objet 
 exports.createSauce = async (req, res, next) => {
 
+    // récupère les information de la sauce dans une variable 
     const Sauces = await JSON.parse(req.body.sauce);
-    delete Sauces._id;
+
+    // crée un nouvel element sauce avec l'url de l'image associer 
     const sauce = new Sauce({
         ...Sauces,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
     });
 
+    // sauvegarde le nouvel element 
     sauce.save()
-
         .then(() => res.status(201).json({ message: 'objet enregistrer' }))
         .catch(error => res.status(400).json({ error }));
 
@@ -27,8 +30,30 @@ exports.createSauce = async (req, res, next) => {
 
 // afficher un objet 
 exports.getOneSauce = (req, res, next) => {
-
+    // recherche la sauce avec l'id associer 
     Sauce.findOne({ _id: req.params.id })
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
+};
+
+// supprimer un element
+exports.deleteSauce = (req, res, next) => {
+
+    // recherche la sauce avec l'id associer 
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => {
+
+            // récupère le nom de l'image associer 
+            const filename = sauce.imageUrl.split('/images/')[1];
+
+            // supprime l'image avec le nom associer dans le dossier image 
+            fs.unlink(`images/${filename}`, () => {
+                // supprime la sauce avec l'id associer 
+                Sauce.deleteOne({ _id: req.params.id })
+                    .then(() => res.status(200).json('objet supprimé !'))
+                    .catch(error => res.status(400).json({ error }));
+
+            })
+        })
+        .catch(error => res.status(500).json({ error }));
 };
