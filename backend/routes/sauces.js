c181f5ae81f86c987c27083f56fd9e2455b30b4c
ThevenonT <thevenon.thomas@hotmail.com>
2022-08-25 11:01:56 +0200

    added the possibility to modify an element and add a like or dislike

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -5,16 +5,22 @@ const sauceCtrl = require('../controllers/sauces');
 const multer = require('../middleware/multer-config');
 
 
-// afficher tous les objet 
+// afficher tous les element  
 router.get('/', auth, sauceCtrl.getAllSauce);
 
-// enregistrer un objet 
+// enregistrer un element  
 router.post('/', auth, multer, sauceCtrl.createSauce);
 
-// afficher un objet 
+// afficher un element 
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 
 // supprimer un element
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
+
+// modifie un element 
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+
+// modifier un like 
+router.post('/:id/like', auth, sauceCtrl.likes);
 
 module.exports = router;
